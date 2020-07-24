efc9595428c3e41ed319ed3a152401e96b8fd8c6
ToDDorov <todorgz7@gmail.com>
2020-07-24 12:16:12 +0300

    Add basic math functions

---
+++ script.js
@@ -0,0 +1,36 @@
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    if (number1 == 0 || number2 == 0) {
+        console.warn("Dividing with 0");
+
+        return null;
+    }
+
+    return number1 / number2;
+}
+
+function operate(operator, number1, number2) {
+    switch (operator) {
+        case "+":
+            return add(number1, number2);
+        case "-":
+            return subtract(number1, number2);
+        case "*":
+            return multiply(number1, number2);
+        case "/":
+            return divide(number1, number2);
+        default:
+            return "No such operator";
+    }
+}
